73024e8eec9a1a96cfa0708e13d00d2e8ae384d6
LuckyDog-LuckyDog <854806207@qq.com>
2019-12-28 13:56:14 +0800

    Site updated: 2019-12-28 13:56:13

--- js/snow2.js
+++ js/snow2.js
@@ -1,4 +1,4 @@
-/*样式一*/
+<!--样式1-->
 <script type="text/javascript" src="http://libs.baidu.com/jquery/1.8.3/jquery.js"></script>
 <script type="text/javascript" src="http://libs.baidu.com/jquery/1.8.3/jquery.min.js"></script>
 (function ($) {
@@ -7,14 +7,14 @@
                 'position': 'absolute',
                 'z-index': '9999',
                 'top': '-50px'
-            }).html('❆'),
+            }).html('&#10052;'),
             documentHeight = $(document).height(),
             documentWidth = $(document).width(),
             defaults = {
                 minSize: 10,
                 maxSize: 20,
                 newOn: 1000,
-                flakeColor: "#3399FF" /* 此处可以定义雪花颜色，若要白色可以改为#FFFFFF */
+                flakeColor: "#3399FF" <!--此处可以定义雪花颜色，若要白色可以改为#FFFFFF >
             },
             options = $.extend({}, defaults, options);
         var interval = setInterval(function () {
@@ -45,6 +45,6 @@ $(function () {
         /* 定义雪花最小尺寸 */
         maxSize: 50,
         /* 定义雪花最大尺寸 */
-        newOn: 200 /* 定义密集程度，数字越小越密集 */
+        newOn: 300 /* 定义密集程度，数字越小越密集 */
     });
 });
